233adc62a20fd0a148668caf5a5342b831502fa7
EC2 Default User <aya420tansk666@gmail.com>
2022-06-07 03:35:57 +0000

    explode_implode.phpを書いた

---
+++ explode_implode.php
@@ -0,0 +1,16 @@
+<?php
+
+// 元の文字列:「;」で区切られていると想定
+$base_string = "Srollen;panettone;Pandoro;Kouglof;Lussekatter";
+echo "元の文字列 \n";
+var_dump($base_string);
+
+//explode()で、;のところで分解
+$exploded_array = explode(";",$base_string);
+echo "\n explodeした配列 \n";
+var_dump($exploded_array);
+
+//implode()で、,(カンマ)で繋げて再組み立て
+$imploded_string = implode(",", $exploded_array);
+echo "\n implodeで繋げた文字列 \n";
+var_dump($imploded_string);
